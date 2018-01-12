e004b8d2a20e9072228ee9bf667240eeaf502aae
GavinCanan <32497075+GavinCanan@users.noreply.github.com>
2018-01-11 23:32:41 -0500

    UNH game lineups

--- lineups.py
+++ lineups.py
@@ -15,6 +15,25 @@ lineup12 = ["Mike Okauru", "Kevaughn Allen", "Jalen Hudson", "Kevarrius Hayes","
 lineup13 = ["Chris Chiozza", "Mike Okauru", "Jalen Hudson", "Kevarrius Hayes","Gorjok Gak",  2]
 lineup14 = ["Chris Chiozza", "Mike Okauru", "Jalen Hudson", "Egor Koulechov","Gorjok Gak",  1]
 lineup15 = ["Chris Chiozza", "Deaundrae Ballard", "Egor Koulechov", "Keith Stone", "Kevarrius Hayes",  1]
+lineup16 = ["Chris Chiozza", "Kevaughn Allen", "Egor Koulechov", "Keith Stone", "Kevarrius Hayes",  1]
+lineup17 = ["Chris Chiozza", "Kevaughn Allen", "Jalen Hudson", "Keith Stone", "Gorjok Gak",  1]
+lineup18 = ["Mike Okauru", "Kevaughn Allen", "Jalen Hudson", "Keith Stone", "Gorjok Gak",  1]
+lineup19 = ["Kevaughn Allen", "Jalen Hudson", "Deaundrae Ballard", "Egor Koulechov", "Gorjok Gak",  1]
+lineup20 = ["Chris Chiozza", "Jalen Hudson", "Deaundrae Ballard", "Egor Koulechov", "Kevarrius Hayes",  1]
+lineup21 = ["Chris Chiozza", "Kevaughn Allen", "Jalen Hudson", "Chase Johnson", "Gorjok Gak",  1]
+lineup22 = ["Mike Okauru", "Jalen Hudson", "Deaundrae Ballard", "Chase Johnson", "Gorjok Gak",  1]
+lineup23 = ["Mike Okauru", "Jalen Hudson", "Deaundrae Ballard", "Chase Johnson", "Kevarrius Hayes",  1]
+lineup24 = ["Kevaughn Allen", "Jalen Hudson", "Deaundrae Ballard", "Chase Johnson", "Kevarrius Hayes",  1]
+lineup25 = ["Kevaughn Allen", "Jalen Hudson", "Deaundrae Ballard", "Chase Johnson", "Kevarrius Hayes",  1]
+lineup26 = ["Chris Chiozza", "Kevaughn Allen", "Jalen Hudson", "Chase Johnson", "Kevarrius Hayes",  1]
+lineup27 = ["Kevaughn Allen", "Jalen Hudson", "Egor Koulechov" , "Kevarrius Hayes","Gorjok Gak",  2]
+lineup28 = ["Kevaughn Allen", "Jalen Hudson", "Deaundrae Ballard", "Kevarrius Hayes","Gorjok Gak",  2]
+lineup29 = ["Chris Chiozza", "Jalen Hudson", "Deaundrae Ballard", "Chase Johnson","Keith Stone",  0]
+lineup30 = ["Chris Chiozza","Kevaughn Allen", "Jalen Hudson", "Deaundrae Ballard","Egor Koulechov",  0]
+lineup31 = ["Chris Chiozza","Kevaughn Allen", "Jalen Hudson", "Egor Koulechov", "Gorjok Gak",  1]
+lineup32 = ["Chris Chiozza","Kevaughn Allen", "Jalen Hudson",  "Keith Stone", "Kevarrius Hayes",  1]
+
+
 
 
 fl_lineups = []
@@ -33,10 +52,72 @@ fl_lineups.append(lineup12)
 fl_lineups.append(lineup13)
 fl_lineups.append(lineup14)
 fl_lineups.append(lineup15)
+fl_lineups.append(lineup16)
+fl_lineups.append(lineup17)
+fl_lineups.append(lineup18)
+fl_lineups.append(lineup19)
+fl_lineups.append(lineup20)
+fl_lineups.append(lineup21)
+fl_lineups.append(lineup22)
+fl_lineups.append(lineup23)
+fl_lineups.append(lineup24)
+fl_lineups.append(lineup25)
+fl_lineups.append(lineup26)
+fl_lineups.append(lineup27)
+fl_lineups.append(lineup28)
+fl_lineups.append(lineup29)
+fl_lineups.append(lineup30)
+fl_lineups.append(lineup31)
+fl_lineups.append(lineup32)
 fl_lineups.append("Florida")
 # After checking if player is in a team's lineup
 # can determine which team by checking last item in list
 
+
+#UNH lineups
+unh_lineup1 = ["Jacoby Armstrong", "Jordan Reed", "Iba Camara", "Elijah Jordan", "Tanner Leissner", 1]
+unh_lineup2 = ["Jacoby Armstrong", "Jordan Reed", "Iba Camara", "David Ogwuche", "Tanner Leissner", 1]
+unh_lineup3 = ["Jacoby Armstrong", "Jordan Reed", "Daryl Stewart", "David Ogwuche", "Tanner Leissner", 0]
+unh_lineup4 = ["David Watkins", "Jordan Reed", "Daryl Stewart", "David Ogwuche", "Tanner Leissner", 0]
+unh_lineup5 = ["David Watkins", "Elijah Jordan", "Daryl Stewart", "David Ogwuche", "Tanner Leissner", 0]
+unh_lineup6 = ["David Watkins", "Elijah Jordan", "Daryl Stewart", "David Ogwuche", "Jacoby Armstrong", 0]
+unh_lineup7 = ["Tanner Leissner", "Elijah Jordan", "Jordan Reed", "David Ogwuche", "Jacoby Armstrong", 0]
+unh_lineup8 = ["Tanner Leissner", "Elijah Jordan", "Jordan Reed", "David Ogwuche", "Iba Camara", 1]
+unh_lineup9 = ["Tanner Leissner", "Daryl Stewart", "Jordan Reed", "David Ogwuche", "Jacoby Armstrong", 0]
+unh_lineup10 = ["Tanner Leissner", "Josh Hopkins", "Jordan Reed", "David Ogwuche", "Jacoby Armstrong", 0]
+unh_lineup11 = ["Tanner Leissner", "Josh Hopkins", "Jordan Reed", "Elijah Jordan", "Jacoby Armstrong", 0]
+unh_lineup11 = ["Tanner Leissner", "David Ogwuche", "Jordan Reed", "Elijah Jordan", "Jacoby Armstrong", 0]
+unh_lineup12 = ["Tanner Leissner", "David Ogwuche", "Jordan Reed", "Elijah Jordan", "David Watkins", 0]
+unh_lineup13 = ["Daryl Stewart", "David Ogwuche", "Jordan Reed", "Luke Rosinski", "David Watkins", 0]
+unh_lineup14 = ["Jacoby Armstrong", "Jordan Reed", "Iba Camara", "Daryl Stewart", "Tanner Leissner", 1]
+unh_lineup15 = ["Jacoby Armstrong", "Jordan Reed", "Iba Camara", "Elijah Jordan", "David Ogwuche", 1]
+unh_lineup16 = ["Tanner Leissner", "Jordan Reed", "Iba Camara", "Elijah Jordan", "David Ogwuche", 1]
+unh_lineup17 = ["Tanner Leissner", "Jordan Reed", "Jacoby Armstrong", "Elijah Jordan", "David Ogwuche", 0]
+unh_lineup18 = ["Iba Camara", "Jordan Reed", "Jacoby Armstrong", "Elijah Jordan", "David Ogwuche", 1]
+unh_lineup19 = ["Tanner Leissner", "Jordan Reed", "David Watkins", "Elijah Jordan", "David Ogwuche", 0]
+unh_lineup20 = ["Tanner Leissner", "Jordan Reed", "Jacoby Armstrong", "Elijah Jordan", "Iba Camara", 1]
+
+unhlineups = []
+unh_lineups.append(unh_lineup1)
+unh_lineups.append(unh_lineup2)
+unh_lineups.append(unh_lineup3)
+unh_lineups.append(unh_lineup4)
+unh_lineups.append(unh_lineup5)
+unh_lineups.append(unh_lineup6)
+unh_lineups.append(unh_lineup7)
+unh_lineups.append(unh_lineup8)
+unh_lineups.append(unh_lineup9)
+unh_lineups.append(unh_lineup10)
+unh_lineups.append(unh_lineup11)
+unh_lineups.append(unh_lineup12)
+unh_lineups.append(unh_lineup13)
+unh_lineups.append(unh_lineup14)
+unh_lineups.append(unh_lineup15)
+unh_lineups.append(unh_lineup16)
+unh_lineups.append(unh_lineup17)
+unh_lineups.append(unh_lineup18)
+unh_lineups.append(unh_lineup19)
+unh_lineups.append(unh_lineup20)
 
 
 #cincinnati Lineups used in game
